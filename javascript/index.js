bb44d741e5da7351ebc108d9bfe16f5c412cbf08
abdurrehmaan11223344 <abdurrehmaan.khalid@thedevnation.com>
2022-02-11 21:17:34 +0500

    javascript lecture1

---
+++ javascript/index.js
@@ -0,0 +1,148 @@
+// console.log("hello world")
+// console.warn("hello world")
+// console.error("hello world")
+
+
+//  var , let , const 
+
+// {
+//     var y=3
+//     console.log("i==> ",y)
+// }
+    
+
+// console.log(y)
+// console.log("g ===> ",x)
+
+// let x = 6;
+// console.log(x)
+// x=19;
+// console.log(x);
+
+
+// const x = 7;
+// console.log(x)
+// x=10;
+
+// number, boolean, null , undefined, string
+// let x = 5.4; 
+// let booleanValue = true;
+// let string= 'this is string'
+// let string1= "this is string"
+// let nullValue = null;
+// let undefinedValue;
+
+
+// console.log(null)
+
+// fname = "Abdur Rehman"
+// lname = 4
+
+// result = `My name is ${fname} and my last name is ${lname}`
+// console.log(result)
+
+// console.log(`My name is ${fname}`)
+// console.log("My Name is "+fname)
+
+// let string1 = "My name Abdur Rehman Khalid and i m a web developer"
+// console.log("Length Prop ==> ",string1.length)
+// console.log(string1.toUpperCase())
+// console.log("substring ==> ",string1.substring(0, 7))
+// console.log(string1.split(" "))
+
+// var x= ['orange', 'apple', 1 ,3, 4 ,2]
+
+// console.log(x)
+// let indexvalue= x.indexOf('apple')
+// console.log(indexvalue)
+
+
+// console.log(x)
+
+// let person=[
+//     {
+//         id: 1,
+//         text: "this is text1",
+//         status: true,
+//     },
+//     {
+//         id: 1,
+//         text: "this is text1",
+//         status: true
+//     },
+//     {
+//         id: 1,
+//         text: "this is text1",
+//         status: true
+//     }
+// ]
+
+// let PersonJson = JSON.stringify(person)
+// console.log(PersonJson)
+
+let person=[
+    {
+        id: 1,
+        text: "this is text1",
+        status: true,
+    },
+    {
+        id: 2,
+        text: "this is text2",
+        status: false
+    },
+    {
+        id: 3,
+        text: "this is text3",
+        status: true
+    },
+    {
+        id: 4,
+        text: "this is text3",
+        status: false
+    }
+]
+
+// Loops
+
+    // person.forEach(function(person){
+    //     console.log(person.id)
+    // })
+
+    
+    // let mapfunction = person.map(function(x){
+    //     return x.id;
+    // })
+
+    // console.log(mapfunction)
+
+
+    // let mapfunction = person.filter(function(x){
+    //     return x.status==false;
+    // })
+
+    // console.log(mapfunction)
+
+    // for(let i=0; i<=person.length; i++ ){
+    //     console.log(person[i].text)
+    // }
+
+    // for(let x of person){
+    //     console.log(x)
+    // }
+
+    // while loop
+
+    // let i=0;
+    // while(i<=10)
+    // {
+    //     console.log(i)
+    //     i++;
+    // }
+
+    
+
+
+
+
+
